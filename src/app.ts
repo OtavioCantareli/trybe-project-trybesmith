c8e49cae5ee9258c9e73d965c794f86eeda6a0e8
Otavio Cantareli <otavio_cantareli@outlook.com>
2022-07-29 21:59:20 -0300

    Add route to app

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import prodRouter from './routes/products';
+import userRouter from './routes/users';
 
 const app = express();
 
 app.use(express.json());
 app.use('/products', prodRouter);
+app.use('/users', userRouter);
 
 export default app;
