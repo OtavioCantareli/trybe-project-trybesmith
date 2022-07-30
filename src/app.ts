1b700cdf7a2de42f96078dbef96de1eb6eabe890
Otavio Cantareli <otavio_cantareli@outlook.com>
2022-07-29 22:29:17 -0300

    Added route

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import prodRouter from './routes/products';
 import userRouter from './routes/users';
+import orderRouter from './routes/orders';
 
 const app = express();
 
 app.use(express.json());
 app.use('/products', prodRouter);
 app.use('/users', userRouter);
+app.use('/orders', orderRouter);
 
 export default app;
