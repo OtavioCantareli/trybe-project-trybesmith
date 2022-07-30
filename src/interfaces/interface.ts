98f48ae198f69e7c55f333de51bd30cf0e84afdb
Otavio Cantareli <otavio_cantareli@outlook.com>
2022-07-29 22:29:39 -0300

    interfaces

--- src/interfaces/interface.ts
+++ src/interfaces/interface.ts
@@ -1,8 +1,16 @@
+import { Request } from 'express';
+
 export interface Product {
   id?: number;
   name: string;
   amount: string;
   orderId?: number;
+}
+
+export interface Order {
+  id?: number;
+  userId: number;
+  productsIds?: Array<number>;
 }
 
 export interface User {
@@ -11,4 +19,13 @@ export interface User {
   classe: string;
   level: number;
   password?: string;
+}
+
+export interface ReqUser extends Request {
+  user?: {
+    id: number;
+    username: string;
+    classe: string;
+    level: number;
+  };
 }
